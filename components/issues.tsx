3380cf24cc9999a308e93222284ab9725595ca98
alexander-fischer <alexanderfischerdev@gmail.com>
2019-11-10 06:38:23 +0100

    fixes

--- components/issues.tsx
+++ components/issues.tsx
@@ -1,4 +1,4 @@
-import IssueModel from "../src/models/issue-model"
+import IssueModel, { ISSUE_TYPE } from "../src/models/issue-model"
 
 interface IIssuesProps {
     issues: IssueModel[]
@@ -9,11 +9,41 @@ const Issues = (props: IIssuesProps) => {
         const lat = issue.latitude.toString().substring(0, 5)
         const lng = issue.longitude.toString().substring(0, 5)
 
+        let type = "unbekannt"
+        switch (issue.issueType) {
+            case ISSUE_TYPE.ANIMAL:
+                type = "Tier"
+                break
+
+            case ISSUE_TYPE.DAMAGED_TREE:
+                type = "Baum beschädigt"
+                break
+
+            case ISSUE_TYPE.HEALTHY_TREE:
+                type = "Gesunder Baum"
+                break
+
+            case ISSUE_TYPE.MISC:
+                type = "Verschiedenes"
+                break
+
+            case ISSUE_TYPE.TREE_DEAD:
+                type = "Toter Baum"
+                break
+
+            case ISSUE_TYPE.TREE_SICK:
+                type = "Kranker Baum"
+                break
+
+            default:
+                break
+        }
+
         return (
             <tr key={index}>
                 <td className="border px-4 py-2">{issue.description}</td>
                 <td className="border px-4 py-2">{`${lat}, ${lng}`}</td>
-                <td className="border px-4 py-2">{issue.issueType}</td>
+                <td className="border px-4 py-2">{type}</td>
             </tr>
         )
     })
