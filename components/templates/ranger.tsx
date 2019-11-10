d48955a86374ab5af9992b63c1af58013470a784
alexander-fischer <alexanderfischerdev@gmail.com>
2019-11-10 05:50:41 +0100

    add issues tsx

--- components/templates/ranger.tsx
+++ components/templates/ranger.tsx
@@ -1,4 +1,5 @@
 import IssueModel from "../../src/models/issue-model"
+import Issues from "../issues"
 
 import dynamic from "next/dynamic"
 const Map = dynamic(
@@ -19,6 +20,7 @@ const RangerTemplate = (props: IRangerTemplateProps) => {
                     width="100%"
                     height="500px" />
             </div>
+            <Issues />
         </div>
     )
 }
